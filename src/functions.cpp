385ed19cc49c69ff0e5a10bea69d697869cc4b14
Luca Teodori <luca95teo@gmail.com>
2019-05-29 03:11:32 +0200

    Managed to develop powerSPectra class

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,26 +1,21 @@
-#include <iostream>
-#include <iomanip>
-#include <fstream>
-
-#include <astro/cosmology/cosmologyBase.h>
-#include <astro/cosmology/cosmicStructures.h>
-#include <astro/io/functionWriter.h>
-#include <astro/utilities/utilities.h>
-
-#include <KFT/particleDynamics/newZeldovichParticleDynamics.h>
-
 #include "../include/powerSpectraTeodori.h"
 
 
-
-Powermine::Powermine(int number_in,  astro::cosmologyBase cos_model ) 
+Powermine::Powermine(int number_in,  astro::cosmologyBase * cos_model ) :
+    m_number_in (number_in), cosmological_model (cos_model)
 {
-    m_number_in=number_in;
         // Cosmology setting
-    astro::cosmicStructures cos_struct (&cos_model); 
-    KFT::newZeldovichParticleDynamics prop (&cos_model, a_min); 
+    cosmic_structures = new astro::cosmicStructures (cosmological_model);
+    propagator = new KFT::newZeldovichParticleDynamics (cosmological_model, a_min);
 
 }
+
+Powermine::~Powermine()
+{
+    delete cosmic_structures;
+    delete propagator;
+}
+
 double Powermine::p_initial (double k)
 {
      switch (m_number_in) {
@@ -53,98 +48,142 @@ double Powermine::sigma_1 ()
 }
 
 // Then for the real Q factor I have to multiply this by sigma_1
-double QFactor (Powermine spectrum, double a, double k)
+double Powermine::QFactor (double a, double k)
 {
-
-    double propagator = spectrum.prop.g_qp(a);
-    double Q = propagator * propagator * 0.33*k*k*spectrum.sigma_1();
+    double prop = propagator->g_qp(a);
+    double Q = prop * prop * 0.33*k*k*sigma_1();
     return Q;
 }
 
 
-double B_1 (Powermine spectrum, double q)
+double Powermine::B_1 (double q)
 {
-    std::function<double (double, double)> p= [spectrum](double q, double k)
-        {return (1/(2*M_PI*M_PI))* spectrum.p_initial(k);};
+    std::function<double (double, double)> p= [this](double q, double k)
+        {return (1/(2*M_PI*M_PI))* p_initial(k);};
     astro::osc_integrator::Integrand p1 =  p;
     astro::osc_integrator Integral	(p1, astro::OSC_SPH_BESSEL,
-        2,spectrum.k_min,spectrum.k_max, astro::LOG_SPACING, spectrum.nbins );
+        2,k_min,k_max, astro::LOG_SPACING, nbins );
     return Integral(q);
 };
 
-double B_2 (Powermine spectrum, double q)
+double Powermine::B_2 (double q)
 {
-    std::function<double (double, double)> p = [spectrum](double q, double k)
-        {return -(1/(2*M_PI*M_PI*k*q))* spectrum.p_initial(k);};
+    std::function<double (double, double)> p = [this](double q, double k)
+        {return -(1/(2*M_PI*M_PI*k*q))* p_initial(k);};
     astro::osc_integrator::Integrand p1 = p;
     astro::osc_integrator Integral	(p1, astro::OSC_SPH_BESSEL,
-        1,spectrum.k_min,spectrum.k_max, astro::LOG_SPACING, spectrum.nbins );
+        1,k_min,k_max, astro::LOG_SPACING, nbins );
     return Integral(q);
 }
 
-double integral_mu (Powermine spectrum, double q, double k)
+double Powermine::integral_mu (double q, double k)
 {
-    std::function<double (double, double)> p = [spectrum, q](double qk, double mu)
-        {return 2 * exp(mu * mu * B_1(spectrum, q)) - 1 ;};
+    std::function<double (double, double)> p = [q, this](double qk, double mu)
+        {return 2 * exp(mu * mu * B_1(q)) - 1 ;};
     astro::osc_integrator::Integrand p1 = p;
     astro::osc_integrator Integral	(p1, astro::OSC_COSINE,
-        0,0,1, astro::LOG_SPACING, spectrum.nbins );
+        0,0,1, astro::LOG_SPACING, nbins );
     return Integral(q);
 }
 
-double integral_q (Powermine spectrum, double k)
+double Powermine::integral_q (double k)
 {
-    std::function<double (double)> p = [spectrum, k](double q)
-        {return q*q * exp(B_2(spectrum,q)) * integral_mu(spectrum,q,k) ;};
+    std::function<double (double)> p = [k, this](double q)
+        {return q*q * exp(B_2(q)) * integral_mu(q,k) ;};
     astro::integrator kernel (p);
-    return kernel(spectrum.q_min, spectrum.q_max);
+    return kernel(q_min, q_max);
 }
-
-
-double preFactorCurlyP(Powermine spectrum, double k, double a)
+double Powermine::CurlyP(double a, double k)
 {
-    double g_qp = spectrum.prop.g_qp(a);
-    return 2* M_PI *exp(-g_qp *g_qp*k*k);
-}
+    double g_qp = propagator->g_qp(a);
+    double prefact = 2* M_PI *exp(-g_qp *g_qp*k*k);
+    return prefact * integral_q(k);
 
-double preFactorGradV(Powermine spectrum, double a, double k)
-{
-    double D_plus = spectrum.cos_struct.Dplus(a);
-    double g = spectrum.prop.g(a)    ;
-    return 3*a*k*k*D_plus * D_plus /(8*M_PI*M_PI*g*g);
 }
-
 
 // Plotting functions
 
-void writeCurlyP1(Powermine spectrum)
+void Powermine::writeCurlyP1()
 {
     int n = 1000;
     std::ofstream openFile;
     openFile.open("curlyP.txt");
     for (int i=0; i< n; i++)
     {
-        double q = astro::x_logarithmic (i, n, spectrum.q_min, spectrum.q_max);
-        openFile << std::setw(15) << q << "\t" << std::setw(15) << B_1(spectrum,q)  << "\t" <<
-            std::setw(15) << B_2(spectrum, q) << std::endl;
+        double q = astro::x_logarithmic (i, n, q_min, q_max);
+        openFile << std::setw(15) << q << "\t" << std::setw(15) << B_1(q)  << "\t" <<
+            std::setw(15) << B_2(q) << std::endl;
     }
 	openFile.close();
 }
 
-void writeCurlyP2(Powermine spectrum, double k)
+void Powermine::writeCurlyP2(double k)
 {
     int n = 100;
     std::ofstream openFile;
     openFile.open("curlyP2.txt");
     for (int i=0; i< n; i++)
     {
-        double q = astro::x_logarithmic (i, n, spectrum.q_min, spectrum.q_max);
+        double q = astro::x_logarithmic (i, n, q_min, q_max);
 
         openFile << std::setw(15) << q << "\t" << std::setw(15)/*  << k  << "\t" <<
-            std::setw(15) */ << integral_mu(spectrum,q,k) << std::endl;
+            std::setw(15) */ << integral_mu(q,k) << std::endl;
     }
 	openFile.close();
 }
+
+// GradV functions
+double J (double y) {return 1 + (1 - y*y)/(2*y) *log((1 + y)/(1-y));}
+
+double Powermine::integral_y (double a, double k)
+{
+    double g_qp = propagator->g_qp(a);
+    std::function<double (double)> p = [this,k,a,g_qp](double y)
+        {return y*y * exp(-sigma_1()*g_qp*g_qp *k*k*y*y/3 ) * p_initial(k*y)*J(y) ;};
+    astro::integrator kernel (p);
+    return kernel(0.001, 0.999);
+}
+
+double Powermine::gradV (double a, double k)
+{
+    double D_plus = cosmic_structures->Dplus(a);
+    double g = propagator->g(a);
+    double prefact = 3*a*k*k*D_plus * D_plus /(8*M_PI*M_PI*g*g);
+    return prefact*integral_y(a,k);
+}
+
+
+// S_I functions
+
+double Powermine::integrand_SI(double a, double k)
+{
+    double gdot = propagator->g_dot(a);
+    double h =  propagator->h(a);
+    double g = propagator->g(a);
+    double prefact =gdot * h/(g*g);
+    std::function<double (double)> p = [this,k](double a1)
+        {return propagator->g(a1) *( propagator->jacobian(a1)/a1) * gradV(a1,k)  ;};
+    astro::integrator kernel (p);
+    return gradV(a,k) + prefact * kernel(0.0, a);
+
+}
+
+double Powermine::S_I (double a, double k)
+{
+    std::function<double (double)> p = [this,a,k](double a1)
+        {return propagator->g_qp(a,a1) *( propagator->jacobian(a1)/a1) * integrand_SI(a1,k)  ;};
+    astro::integrator kernel (p);
+    return 2*k*kernel(0,a);
+}
+
+double Powermine::fullPowerSpectrum (double a, double k)
+{
+    double Q_D = QFactor(a,k);
+    double curlyP = CurlyP(a,k);
+    double SI = S_I(a,k);
+    return exp(-Q_D + SI) * curlyP;
+}
+
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,37 @@
-#include <iostream>
-#include <iomanip>
-#include <fstream>
-
-#include <astro/cosmology/cosmologyBase.h>
-#include <astro/cosmology/cosmicStructures.h>
-#include <astro/io/functionWriter.h>
-#include <astro/utilities/utilities.h>
-
-#include <KFT/particleDynamics/newZeldovichParticleDynamics.h>
-
 #include "../include/powerSpectraTeodori.h"
 
 int main ()
 {
     astro::cosmologyBase cos_model;
-    Powermine spectrum (4, cos_model);
-    double prefact = preFactorGradV(spectrum, 0.5,10);
-    std::cout << prefact << std::endl;
+    Powermine spectrum (4, &cos_model);
+    double a = 0.1;
+    double k = 10;
+    double q = 3;
 
+    double Qtest = spectrum.QFactor(a,k);
+    double p_initialTest = spectrum.p_initial(k);
+    double sigma_test = spectrum.sigma_1();
+    double B1Test = spectrum.B_1(q);
+    double B2Test = spectrum.B_2(q);
+    double muTest = spectrum.integral_mu(q,k);
+    // double qTest = spectrum.integral_q(k);
+    // double curlyPtest = spectrum.CurlyP(a,k) ;
+    double IntegralYTest = spectrum.integral_y(a,k) ;
+    double gradVTest  = spectrum.gradV(a,k);
+    double Integrand_SITest = spectrum.integrand_SI(a,k) ;
+    double SITest = spectrum.S_I(a,k) ;
+    std::cout << "Test Q factor:    "  << "\t" << Qtest << std::endl;
+    std::cout << "Test p initial:   " << "\t" << p_initialTest << std::endl;
+    std::cout << "Test sigma_1:     "  << "\t" << sigma_test << std::endl;
+    std::cout << "Test B_1 function:"  << "\t" << B1Test << std::endl;
+    std::cout << "Test B_2 function:"  << "\t" << B2Test << std::endl;
+    std::cout << "Test integral mu: "  << "\t" << muTest << std::endl;
+    // std::cout << "Test integral q:"  << "\t" << qTest << std::endl;
+    // std::cout << "Test curlyP :     "  << "\t" << curlyPtest  << std::endl;
+    std::cout << "Test integral y:  "  << "\t" << IntegralYTest  << std::endl;
+    std::cout << "Test gradient V:  "  << "\t" << gradVTest << std::endl;
+    std::cout << "Test integrand S_I:"  << "\t" << Integrand_SITest  << std::endl;
+    std::cout << "Test S_I:          "  << "\t" << SITest << std::endl;
 
 
 
